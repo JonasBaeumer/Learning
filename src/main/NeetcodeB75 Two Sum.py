a75b54f0e5a800792a0e8e49c76c068c9aa77857
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-10 20:44:09 +0200

    Update NeetcodeB75 Two Sum.py

--- src/main/NeetcodeB75 Two Sum.py
+++ src/main/NeetcodeB75 Two Sum.py
@@ -1,3 +1,21 @@
+# Use a dict to keep store of what we have already seen
+# Runtime: O(n)
+# Spacetime: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        memory_dict = {}
+
+        for index, number in enumerate(nums):
+            
+            # Check if target-number already key in memory
+            if (target - number) in memory_dict:
+                return [memory_dict[target - number], index]
+
+            # If not add it
+            memory_dict[number] = index
+
 # Naive approach
 # Runtime: O(n) x O(n) = O(n^2)
 # Spacetime: O(1)
