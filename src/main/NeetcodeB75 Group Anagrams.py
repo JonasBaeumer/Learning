c32e39b68532fc7462662e4017cfab2a9e25152a
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-10 22:00:45 +0200

    Update NeetcodeB75 Group Anagrams.py

--- src/main/NeetcodeB75 Group Anagrams.py
+++ src/main/NeetcodeB75 Group Anagrams.py
@@ -1,3 +1,28 @@
+# Use the ord function which gives the unicode function to normalize it to create a positional array to allow counting how many times each character occurs
+# Runtime: O(n) + O(m) = O (n + m)
+# Spacetime: O(m)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # Keep a hashmap of store of sorted anagrams
+        memory = {}
+
+        # Loop over strs and sort each string on appearance
+        for string in strs:
+
+            count = [0] * 26
+            for i in range(len(string)):
+                index = ord(string[i]) - ord('a')
+                count[index] += 1
+
+            if "".join(str(count)) in memory:
+                memory["".join(str(count))].append(string)
+            else:
+                memory["".join(str(count))] = [string]
+        
+        return list(memory.values())
+
 # Naive approach
 # Runtime: O(n) x O(1) x O(k log k) sorting -> O(n k log(k))
 # Spacetime: O(n * k)
