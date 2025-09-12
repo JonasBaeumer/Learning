becbbb15e12381a06041944f4cbe54c16bd8c9b0
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-12 19:50:13 +0200

    Update NeetcodeB75 Top K Frequent Elements.py

--- src/main/NeetcodeB75 Top K Frequent Elements.py
+++ src/main/NeetcodeB75 Top K Frequent Elements.py
@@ -1,3 +1,23 @@
+# Runtime: O(n) + O(n) + O(k) = O(2n + k) = O(n)
+# Spacetime: O(n) + O(2k) + O(k) = O(n + 3k) = O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
 # Naive approach
 # Runtime: O(n) + O(n log n) + O(k) = O(n log n + k)
 # Spacetime: O(n) + O(k) = O(n + k)
