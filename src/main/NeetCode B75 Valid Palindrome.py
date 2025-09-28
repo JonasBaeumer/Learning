ecf407f95c0b55c6d48aa0d1eeb6781f604d3e9f
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-28 21:33:23 +0200

    Update NeetCode B75 Valid Palindrome.py

--- src/main/NeetCode B75 Valid Palindrome.py
+++ src/main/NeetCode B75 Valid Palindrome.py
@@ -1,3 +1,22 @@
+# Optimized solution: Do check for valid character at runtime to reduce required space to O(1)
+# Spacecomplexity: O(1)
+# Runtime: O(n)
+class Solution:
+    is_alnum = re.compile(r'[A-Za-z0-9]').match          
+
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not self.is_alnum(s[i]):
+                i += 1
+            while i < j and not self.is_alnum(s[j]):
+                j -= 1
+            if i < j and s[i].casefold() != s[j].casefold():
+                return False
+            i += 1
+            j -= 1
+        return True
+
 # Spacecomplexity: n (new version of the string) + 2 (for each pointer) = n
 # Runtime:  n/2 + n/2 + n (dont now how long sub and lower methods needs (probably need to iterate over whole string) = n
 
