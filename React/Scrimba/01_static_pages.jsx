4cc156d20cff3ef15d94873bf704aca7fd6ab5a1
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-09-30 20:36:33 +0200

    Update 01_static_pages.jsx

--- React/Scrimba/01_static_pages.jsx
+++ React/Scrimba/01_static_pages.jsx
@@ -1,3 +1,20 @@
+
+/**
+ * Challenge: set up a new React app from scratch!
+ * Hints:
+ * 1. Need to import the method to create a new root 
+ *    from the "react-dom/client" package)
+ * 2. Use that root to render some JSX. For now, just render
+ *    an h1 element that says anything you want.
+ */
+
+import {createRoot} from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <h1>Anything I want </h1>
+)
+
 /* 
 Challenge - recreate the above line of code in vanilla JS by creating and
 appending an h1 to our div#root (without using innerHTML).
