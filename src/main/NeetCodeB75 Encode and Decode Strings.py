2fd8090fe9d2151b64e9a060c5846e655769213c
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-03 19:50:19 +0200

    Update NeetCodeB75 Encode and Decode Strings.py

--- src/main/NeetCodeB75 Encode and Decode Strings.py
+++ src/main/NeetCodeB75 Encode and Decode Strings.py
@@ -1,5 +1,7 @@
 # Naive approach: We iterate over every array field, slice it together with a 
 # special sign and then go through the string and slice it up at the special sign
+# Time complexity: O(m) + O(n) = O(m+n) (m length array, n length string)
+# Space complexity: O(m) + O(n) = O(m+n)
 
 class Solution:
 
