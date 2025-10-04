bc0df343cefed66c0b321bc89a5a15377aee5108
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-04 09:36:35 +0200

    Update 01_static_pages.jsx

--- React/Scrimba/01_static_pages.jsx
+++ React/Scrimba/01_static_pages.jsx
@@ -1,4 +1,31 @@
 import { createRoot } from "react-dom/client"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./Main"
+const root = createRoot(document.getElementById("root"))
+
+/**
+ * Challenge: move the MainContent and Footer components to their own
+ * separate files.
+ */
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+
+root.render(
+    <Page />
+)
+
+
+import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
 /**
