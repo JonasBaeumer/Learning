7bf458731248dbf6087af397c3b9aec77d5b94d8
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-05 15:44:12 +0200

    Update 01_static_pages.jsx

--- React/Scrimba/01_static_pages.jsx
+++ React/Scrimba/01_static_pages.jsx
@@ -1,3 +1,25 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="nav-bar">
+                <img src="react-logo.png" className="image" alt="React logo"/>
+                <p>ReactFacts</p>
+            </nav>
+        </header>
+    )
+}
+
 import { createRoot } from "react-dom/client"
 import Header from "./Header"
 import Footer from "./Footer"
