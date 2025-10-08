79e2b4bd702148f216368fe5b060573ed6734248
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-08 19:02:35 +0200

    Create Interests.jsx

---
+++ React/Scrimba/project/01_virtual_businesscard/components/Interests.jsx
@@ -0,0 +1,11 @@
+export default function Interests() {
+    return (
+        <>
+            <h3>Interests</h3>
+            <p className="interests-text"> 
+                I am actually interested in many things. In general, I love problem solving so this is one of things I enjoy most. Furthermore, I am always happy to be outside and be active sports wise (surfing, beachvolleyball). 
+            </p>
+        </>
+        
+    )
+}
