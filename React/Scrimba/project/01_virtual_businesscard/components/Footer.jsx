7938774e439678dbf0ee6e165e7e6704c91ecfbd
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-08 19:01:59 +0200

    Create Footer.jsx

---
+++ React/Scrimba/project/01_virtual_businesscard/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <img src="./ressources/twitter.png" className="footer-icon" alt="Twitter Logo"/>
+            <img src="./ressources/github.png" className="footer-icon" alt="Github Logo"/>
+            <img src="./ressources/instagram.png" className="footer-icon" alt="Instagram Logo"/>
+            <img src="./ressources/facebook.png" className="footer-icon" alt="Facebook Logo"/>
+        </footer>
+    )
+}
