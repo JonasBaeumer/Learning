61a94444429871b7c568ade72536b6ca8551484c
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-08 19:01:43 +0200

    Create About.jsx

---
+++ React/Scrimba/project/01_virtual_businesscard/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <>
+            <h3>About</h3>
+            <p className="about-text">
+                Im an aspiring developer and this is my first little web project. I am a strong builder with experience in Agentic AI, fast prototyping, Optimization, and Machine Learning. However I realized that something is missing, building. Therefore I am now learning web development in react to complement my skills in this domain. 
+            </p>
+        </>
+    )
+}
