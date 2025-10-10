b5aacaf8b8fee6077236a892dfc020802f3ab108
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-10 09:41:53 +0200

    Update NeetCodeB75 Container with most water.py

--- src/main/NeetCodeB75 Container with most water.py
+++ src/main/NeetCodeB75 Container with most water.py
@@ -1,4 +1,28 @@
 # volume = min(bar[i], bar[j]) * dist(bar[i], bar[j])
+# Optimized approach: Two pointers
+# Idea: Our limiting factor is min(bar[i], bar[j]) so we want to ensure that we want to change 
+# the smaller numbers to see a potential change,
+# Runtime: O(n)
+# Spacetime: O(3) -> O(1) 
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights)-1
+        highest_amount = 0
+        
+        while (left < right):
+            total = min(heights[left], heights[right]) * (right - left)
+            if (total > highest_amount):
+                highest_amount = total
+            if (heights[left] < heights[right]):
+                left+=1
+            else:
+                right -=1
+
+        return highest_amount
+
+# volume = min(bar[i], bar[j]) * dist(bar[i], bar[j])
 # Naive approach: Use nested for loop to discover all options
 # Runtime: O(n) * O(n) = O(n^2)
 # Spacetime: O(1) 
