bb51400a22ca1c203827b02080c186fb752c1244
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-14 16:48:54 +0200

    Update 02_data_driven_react.jsx

--- React/Scrimba/02_data_driven_react.jsx
+++ React/Scrimba/02_data_driven_react.jsx
@@ -1,3 +1,20 @@
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  const firstName = "Joe"
+  const lastName = "Schmoe"
+  
+  /**
+   * Challenge: finish off the h1 below so it says "Hello Joe Schmoe"
+   */
+  
+  return (
+    <h1>Hello {firstName + " " + lastName}</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
 /**
  * Challenge: Build out the Entry component and render 1 instance of it
  * to the App
