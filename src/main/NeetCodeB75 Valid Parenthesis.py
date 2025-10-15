895f5ad632c2316c76d7e2b0609deb1866eccc8e
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-15 10:17:13 +0200

    Update NeetCodeB75 Valid Parenthesis.py

--- src/main/NeetCodeB75 Valid Parenthesis.py
+++ src/main/NeetCodeB75 Valid Parenthesis.py
@@ -1,4 +1,26 @@
 # Optimized approach:
+# Same approach, just small code rework and hashmap optimization
+# Runtime: O(n) one iterations through list
+# Spacetime: O(n) worst case size of stack
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+        valid_brackets = {"]":"[", "}":"{", ")":"("}
+
+        for char in s:
+            if char not in valid_brackets.keys():
+                stack.append(char)
+            else:
+                if len(stack) > 0:
+                    if valid_brackets[char] != stack.pop():
+                        return False
+                else:
+                    return False
+        
+        return True if not stack else False
+
+# Optimized approach:
 # If open bracket add to stack
 # If closed bracket: Check if stack empty -> False, if at the end of string len(stack) > 0 -> False
 # Runtime: O(n) one iterations through list
