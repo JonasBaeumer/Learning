b0c2c5f988d3edb436486fdeb3191fe8da93dda2
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-15 11:15:07 +0200

    Update NeetCodeB75 FindMinimumInRotatedSortedArray.py

--- src/main/NeetCodeB75 FindMinimumInRotatedSortedArray.py
+++ src/main/NeetCodeB75 FindMinimumInRotatedSortedArray.py
@@ -1,3 +1,25 @@
+# Optimized approach: Use adjusted version of binary search to solve this
+# Essentially: When mid > right we move to right, when mid < right re move to left
+# Runtime: O(log n)
+# Spacetime: O(1)
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+
+        mid = 0
+        left = 0
+        right = len(nums)-1
+
+        while (left < right):
+            mid = left + (right - left) // 2
+
+            if(nums[mid] > nums[right]):
+                left = mid+1
+            else:
+                right = mid
+
+        return nums[left]
+
 # Trivial solution: Simple loop
 # Runtime: O(n)
 # Spacetime: O(1)
