8b78bc924f88e8812d62b953b94946ebbd140921
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-17 10:16:13 +0200

    Refactor LinkedList cycle detection implementation
    Refactor cycle detection logic for clarity and maintainability.

--- src/main/NeetCodeB75 LinkedList Cycle Detection.py
+++ src/main/NeetCodeB75 LinkedList Cycle Detection.py
@@ -1,5 +1,29 @@
 # Optimized approach: Use two pointers who run at different speeds to track wether
 # there is a cycle or not, if there is they will overlap at some point
+# Runtime: O(n)
+# Spacetime: O(1) 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        pointer1 = head
+        pointer2 = head
+
+        while pointer1 and pointer1.next:
+            pointer1 = pointer1.next.next
+            pointer2 = pointer2.next
+            if pointer1 == pointer2:
+                return True
+
+        return False
+
+# Optimized approach: Use two pointers who run at different speeds to track wether
+# there is a cycle or not, if there is they will overlap at some point
 # Same code just a bit rewritten
 # Runtime: O(n)
 # Spacetime: O(1) 
