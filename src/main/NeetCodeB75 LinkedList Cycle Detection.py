fef8848d187fd96f5004821ef426d0731895d396
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-17 10:11:05 +0200

    Update NeetCodeB75 LinkedList Cycle Detection.py

--- src/main/NeetCodeB75 LinkedList Cycle Detection.py
+++ src/main/NeetCodeB75 LinkedList Cycle Detection.py
@@ -1,3 +1,31 @@
+# Optimized approach: Use two pointers who run at different speeds to track wether
+# there is a cycle or not, if there is they will overlap at some point
+# Runtime: O(n)
+# Spacetime: O(1) 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        pointer1 = head
+        pointer2 = head
+
+        while(pointer1):
+            pointer1 = pointer1.next
+            if pointer1:
+                pointer1 = pointer1.next
+            pointer2 = pointer2.next
+            if (pointer1 is None):
+                return False
+            if (pointer1 == pointer2):
+                return True
+
+        return False
+
 # Initial approach: Store elements in memory to remember what we have already seen
 # Runtime: O(n)
 # Spacetime: O(n) -> need to store every seen node.
