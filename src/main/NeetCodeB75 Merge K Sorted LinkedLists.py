03f67ddd95381b89345619f91db7b177b8d8da94
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-18 14:22:31 +0200

    Update NeetCodeB75 Merge K Sorted LinkedLists.py

--- src/main/NeetCodeB75 Merge K Sorted LinkedLists.py
+++ src/main/NeetCodeB75 Merge K Sorted LinkedLists.py
@@ -1,3 +1,56 @@
+# Other approach: Merge two Linked Lists repeatedly
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Optimized approach: Our outer loop can not be improved since we need to make sure that we have
+# looked at each element in lists at least once since it needs to be appended
+# Inner loop however is inefficient, we are doing a lot of duplicate work since we are only changing
+# a single pointer each iteration while the rest stays the same
+# Optimize this with a min heap/priority queue -> O log k
+# Runtime: O(n) * O(log kk) = k number of lists in lists, n number of nodes that I am appending in total
+# Spacetime: O(k) for heap
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        
+        # Merge the two lists together
+        while(len(lists)) > 1:
+            merged_lists = []
+
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i+1] if (i+1) < len(lists) else None
+                merged_lists.append(self.mergeLists(list1, list2))
+            lists = merged_lists
+        
+        return lists[0]
+
+    
+    def mergeLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy 
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+        
+        return dummy.next
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
