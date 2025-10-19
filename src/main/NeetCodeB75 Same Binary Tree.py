39e2511284ae6705912bbd74f49ec17b2f1d50d3
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-19 12:03:59 +0200

    Add implementation for checking same binary tree

---
+++ src/main/NeetCodeB75 Same Binary Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Base case: Reached leaf node on both trees -> return true
+# BFS to search through the tree
+# Runtime: O(n)
+# Spacecomplexity: O(n)
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
