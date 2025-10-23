f3872b77bbbec47ad3726b89357a1415e6127abc
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-23 11:14:23 +0200

    Update and rename NeetCodeB75 kth smallest integer in BST.py to NeetCodeB75 Kth smallest integer in BST.py

--- src/main/NeetCodeB75 Kth smallest integer in BST.py
+++ src/main/NeetCodeB75 Kth smallest integer in BST.py
@@ -5,6 +5,34 @@
 #         self.left = left
 #         self.right = right
 
+# Traverse the tree with BFS to ensure that we start at the very left side before the right
+# This way we keep track of all the values to the left and the tree is sorted
+# Runtime: O(n)
+# Spacetime: O(n)
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+   
+        value_list = []
+
+        def dfs(root: Optional[TreeNode]):
+            if root is None: 
+                return 
+            dfs(root.left)
+            value_list.append(root.val)
+            dfs(root.right)          
+
+        dfs(root)
+        
+        return value_list[k-1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 # Get all values in this BST with BFS, then sort the list and return index k-1
 # Runtime: O(n log n), extra log n for sorting
 # Spacetime: O(n)
