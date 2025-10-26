5c90df58836a75e6c788ea445e1d03e06a813bd9
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-10-26 19:35:32 +0100

    Update NeetCodeB75 Clone Graph.py

--- src/main/NeetCodeB75 Clone Graph.py
+++ src/main/NeetCodeB75 Clone Graph.py
@@ -5,6 +5,49 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
+# For BFS the trick is different, here we have to make sure by the time we see the node in the original
+# graph our copies of the node are already created. 
+# That means we have to create our original copies in the hashtable the first time we encounter them
+# and that is when we look at the neighborhood of its adjacent nodes.
+# Make sure that you also check that for the very first node! Or otherwise the first node will only
+# be connected one directional so the first node doesnt know the rest of the graph exists 
+# Runtime: O(V+E), we have to iterate over all nodes, and visit all its neighbors (edges)
+# Spacecomplexity: O(V) for the hashmap that stores references to the copies 
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+         # Do it with DFS
+
+         # Traverse the graph with dfs for each point add a new node to our graph 
+         # Also here remember which nodes we have already visited so that we dont repeat ourselves
+        if node is None:
+            return node
+
+        hashmap_old_new = {}
+        queue = deque()
+        queue.append(node)
+
+        while queue:
+            current_node = queue.pop()
+            if current_node not in hashmap_old_new:
+                hashmap_old_new[current_node] = Node(current_node.val)
+            for neighbor in current_node.neighbors:
+                # If we see a neighbor for the first time, create node and add it to queue and hm
+                if neighbor not in hashmap_old_new.keys():
+                    hashmap_old_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                # Regardless, connect the current clone to the neighbor’s clone
+                hashmap_old_new[current_node].neighbors.append(hashmap_old_new[neighbor])
+
+        return hashmap_old_new[node]  
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 # Traverse the graph with DFS (dont need to do level by level however here)
 # Make sure to check wether node has already been visited to avoid loops
 # Current problem: How do I deal with the issue of making actual copies or neighbors, 
