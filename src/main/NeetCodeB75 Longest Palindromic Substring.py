a5a96b5a3a959c43fe42270dc3f077c3d949b8d0
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-11-11 20:26:35 +0100

    Update NeetCodeB75 Longest Palindromic Substring.py

--- src/main/NeetCodeB75 Longest Palindromic Substring.py
+++ src/main/NeetCodeB75 Longest Palindromic Substring.py
@@ -1,3 +1,46 @@
+# New approach, we can consider each string to be the middle of the palindrome
+# and then run outwards in both direction, must consider differnce between 
+# odd and even length
+# Runtime: O(n^2)
+# Spacetime: O(1)
+
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+
+        res = ""
+        res_len = 0
+
+        for i in range(len(s)):
+            l, r = i, i
+
+            # Move pointer outwards (odd numbers)
+            while(l >= 0 and r < len(s)):
+                if(s[l] != s[r]):
+                    break
+                
+                if(len(s[l:r+1]) > res_len):
+                    res = s[l:r+1]
+                    res_len = len(res)
+                
+                l -= 1
+                r += 1
+            
+            # Now check for even palindroms
+            l, r = i, i+1
+            while(l >= 0 and r < len(s)):
+                if(s[l] != s[r]):
+                    break
+                
+                if(len(s[l:r+1]) > res_len):
+                    res = s[l:r+1]
+                    res_len = len(res)
+                
+                l -= 1
+                r += 1
+
+        return res
+
 # Runtime: O(n^3) - we can have up to n^2 substrings, and palindrome check takes O(n)
 # Space: O(n) - depth of recursion stack
 # Problem: Our palindrome check gives us an extra O(n) runtime, so we end up with O(n^3)
