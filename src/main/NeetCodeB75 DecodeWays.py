cb6e1ce686aa01e89fa686c82934985c579556af
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-11-12 09:54:22 +0100

    Update NeetCodeB75 DecodeWays.py

--- src/main/NeetCodeB75 DecodeWays.py
+++ src/main/NeetCodeB75 DecodeWays.py
@@ -1,3 +1,23 @@
+from functools import cache
+
+class Solution:
+    @cache
+    def numDecodings(self, s: str) -> int:
+
+        # Base cases: 
+        # - Current string is empty
+        # - One character left (only one option)
+        # - String leads with 0, not allowed
+        if not s or (len(s) <= 1 and s[0] != "0"):
+            return 1
+        elif s[0] == "0":
+            return 0
+        
+        result = self.numDecodings(s[1:len(s)]) 
+        if len(s) >= 2 and 10 <= int(s[:2]) <= 26:
+            result += self.numDecodings(s[2:])
+        return result
+
 # Runtime: O(n), each substring is computed once due to memoization
 # Space: O(n), recursive stack depth + cache storage
 
