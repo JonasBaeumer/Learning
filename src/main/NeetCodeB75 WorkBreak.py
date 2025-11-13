f062bb90d09db000ae8ec29a65c3a48ee5dd2cb0
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-11-13 16:50:40 +0100

    Create NeetCodeB75 WorkBreak.py

---
+++ src/main/NeetCodeB75 WorkBreak.py
@@ -0,0 +1,59 @@
+# Now instead of trying to create every possible string we see that for the current position 
+# if the string we are at, if there is a string in our dict that we can use 
+# to here to get that substring, if not there is no possible solution and we can stop
+# Runtime: O(n * m * s), n length of string, m number of available words, t max length of any word in dict
+# Spacetime: O(n), depth of recursive call stack, + cache
+
+from functools import cache
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        @cache
+        def dfs(index):
+            
+            # Base case1: we have reached the end of the string we are done
+            if index >= len(s):
+                return True
+
+            # For each word in wordDict -> explore next option and move index
+        
+            for word in wordDict:
+                if s[index:index+len(word)] != word:
+                    continue
+                if dfs(index + len(word)):
+                    return True
+            
+            return False
+        
+        return True if dfs(0) else False
+
+  rom functools import cache
+
+# What is the issue on this solution: When we have long overlapping string until we get to a solution
+# we generate long overlapping pairs of strings that we dont need and that kill our solution / memory.
+# Essentially we are asking ourselves the wrong questions here: We are asking: "Which words can I build with
+# this dict?", but we should ask: "Given the current position I am at is there a word in the dict that 
+# matches it"
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        @cache
+        def dfs(generated_str):
+            
+            # Base case1: String matches perfectly, found valid solution
+            if generated_str == s:
+                return 1
+
+            # Base case2: We have overshot string length, so done exploring
+            if len(generated_str) >= len(s):
+                return 0
+
+            # For each word in wordDict -> add word to dict and explore subproblem
+            result = 0
+            for word in wordDict:
+                result = max(result, dfs(generated_str + word))
+            
+            return result
+        
+        return True if dfs("") else False
