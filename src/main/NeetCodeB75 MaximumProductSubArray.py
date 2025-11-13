e8f5129c324a8b741e3b835ef7f0e355d09ec777
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-11-13 10:53:40 +0100

    Update NeetCodeB75 MaximumProductSubArray.py

--- src/main/NeetCodeB75 MaximumProductSubArray.py
+++ src/main/NeetCodeB75 MaximumProductSubArray.py
@@ -1,4 +1,29 @@
+# Approach we are iteratively going through the row of numbers and multiply them
+# If we happen to hit a 0 at some point we will automatically reset the computation to start again
+# To prevent information loss when having + and -, we store both value 
+# Runtime: O(n)
+# Spacetime: O(1)
 
+class Solution:
+    
+    def maxProduct(self, nums: List[int]) -> int:
+
+        max_so_far, cur_max, cur_min = nums[0], nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            current = nums[i]
+
+            # If current is number is negative this invert our order max is min and vice versa
+            if current < 0:
+                cur_max, cur_min = cur_min, cur_max
+            
+            cur_max = max(current, current * cur_max)
+            cur_min = min(current, current * cur_min)
+
+            max_so_far = max(max_so_far, cur_max)
+            
+        return max_so_far
+        
 
 # Why this solution fails, we have to be careful with positive and negative numbers and how they switch when being multiplied.
 # Suddendly the smallest number multiplied with - can become the biggest and vice versa, therefore in the recursion we are 
