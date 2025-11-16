1f213c82a73f71eb1736441337c6baded4954749
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-11-16 13:25:39 +0100

    Update ValidWordAbbreviation.py

--- src/main/datadog/ValidWordAbbreviation.py
+++ src/main/datadog/ValidWordAbbreviation.py
@@ -1,3 +1,44 @@
+# Runtime O(n)
+# Space: O(1), only two numbers and temp numbers
+
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+
+        i = 0
+        j = 0
+
+        while i < len(abbr):
+
+            # Is the current char a digit?
+            if abbr[i].isdigit():
+                # Check if leading 0 -> Yes, return False
+                if abbr[i] == "0":
+                    return False
+
+                # Build the full number
+                number = 0
+                while i < len(abbr) and abbr[i].isdigit():
+                    number = number * 10 + int(abbr[i])
+                    i += 1
+
+                # Move j pointer up by number
+                j += number
+
+                # Check if j > len(word) -> Yes, return False 
+                if j > len(word):
+                    return False
+
+            # If not check if they are equal and move both pointers
+            else:
+
+                if j >= len(word) or abbr[i] != word[j]:
+                    return False
+
+                i += 1
+                j += 1
+        
+        return j == len(word)
+
 # THIS SOLUTION DOESNT WORK
 
 class Solution:
