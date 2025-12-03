e647a10df423789cacb1cc02b57edc1dfb148306
Jonas <71970468+JonasBaeumer@users.noreply.github.com>
2025-12-03 14:55:07 +0100

    Update NeetCodeB75 ReverseBits.py

--- DSAndAlgos/neetcodeB75/NeetCodeB75 ReverseBits.py
+++ DSAndAlgos/neetcodeB75/NeetCodeB75 ReverseBits.py
@@ -1,3 +1,16 @@
+# Optimal solution, not from me but what is happening here? 
+# I think this is trying to shift different parts of the bit row at different times but not sure
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = n
+        res = (res >> 16) | (res << 16) & 0xFFFFFFFF
+        res = ((res & 0xff00ff00) >> 8) | ((res & 0x00ff00ff) << 8)
+        res = ((res & 0xf0f0f0f0) >> 4) | ((res & 0x0f0f0f0f) << 4)
+        res = ((res & 0xcccccccc) >> 2) | ((res & 0x33333333) << 2)
+        res = ((res & 0xaaaaaaaa) >> 1) | ((res & 0x55555555) << 1)
+        return res & 0xFFFFFFFF
+
 # Input: 21 -> 00000000000000000000000000010101
 # Max - Input: 11111111111111111111111111101010
 
