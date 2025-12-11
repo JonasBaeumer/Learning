442892072a7320ff8cb9fc1f199d71db0cdf3a0f
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-12-11 10:40:57 +0100

    intermediate solution for part II, doesnt work right now

--- AdventOfCode2025/day3.py
+++ AdventOfCode2025/day3.py
@@ -18,25 +18,56 @@ def highest_voltage(bank):
 			highest_value = number
 	return (highest_value, second_highest)
 
-test_string = "818181911112111"
-print(highest_voltage(test_string))
+def add_and_shift(numbers, value, remaining, k):
+	"""
+	numbers: current chosen digits (as a list of ints), in order
+	value:   new digit (int) from the bank we are considering
+	remaining: how many digits are left AFTER this one in the string
+	k:       target length (here: 12)
+	"""
+	# While:
+	# - we have at least one digit in numbers,
+	# - if we keep the current last digit AND all remaining digits,
+	#   we would still have more than k total → so we are allowed to drop one,
+	# - and the last digit is smaller than the new digit,
+	#   then drop the last digit to make room for a better (bigger) one.
+	while numbers and len(numbers) + remaining > k and numbers[-1] < value:
+		numbers.pop()
+
+	# If we still don't have k digits yet, append this one.
+	if len(numbers) < k:
+		numbers.append(value)
+
+	return numbers
+
+def highest_voltage_2(bank: str) -> int:
+	k = 12
+	numbers = []
+	n = len(bank)
+
+	for i, ch in enumerate(bank):
+		digit = int(ch)
+		remaining = n - i - 1  # digits left *after* this one
+		numbers = add_and_shift(numbers, digit, remaining, k)
+
+		# numbers now holds exactly k digits forming the maximum possible number
+	return int("".join(str(d) for d in numbers))
+
+test_string = "987654321111111"
+print(highest_voltage_2(test_string))
 test_string_2 = "811111111111119"
-print(highest_voltage(test_string_2))
+print(highest_voltage_2(test_string_2))
 test_string_3 = "234234234234278"
-print(highest_voltage(test_string_3))
+print(highest_voltage_2(test_string_3))
 test_string_4 = "818181911112111"
-print(highest_voltage(test_string_4))
-test_string_5 = "8553334231333333322333224115242343435135523343222332535523213448433134323333943412253333331323323432"
-print(highest_voltage(test_string_5))
-test_string_6 = "2221235222332215222222222212722222334222723221322222522222122222423212222222122124353332123442222223"
-print(highest_voltage(test_string_6))
+print(highest_voltage_2(test_string_4))
 
 filepath = "/Users/jonas/Downloads/input-3.txt"
 total_sum = 0
 with open(filepath, "r") as f:
 	for line in f:
 		line = line.strip()
-		n1, n2 = highest_voltage(line)
-		total_sum += (n1 * 10 + n2)
+		number = highest_voltage_2(line)
+		total_sum += number
 print(total_sum)
 		
