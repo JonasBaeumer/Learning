5ac2b9b510fb21aaf13f96719b36e63fa09d7c5b
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-12-22 17:27:24 +0100

    added working solution for day5 part 2

--- AdventOfCode2025/day5.py
+++ AdventOfCode2025/day5.py
@@ -24,6 +24,37 @@ test_range = [(27,273), (300, 320)]
 test_id = 302	
 print(check_ingredient_fresh(test_id, test_range))
 
+"""
+Calculate all fresh IDs that are contained in total
+Important: The ranges can overlap so we need to merge overlapping intervalls first
+We have sorted the list therefore we know that out starting points must be at least equal, since the right side is bigger that 
+leads to two possible scenarios:
+A) The intervals do not overlap 
+B) The intervals partially overlap. What does that mean?
+	If the intervals partially overlap that means that the starting point and/or the endpoint are within the original interval
+	If they are there are two possibilities: Either the interval stays the same (since both start and end are contained within interval)
+	or we grow the right side of the inveral if the end of the new one is bigger 
+"""
+def calculate_total_fresh_ids(id_range: list([int, int])) -> int:
+	sum = 0
+	left,right = (id_range[0][0], id_range[0][1] + 1)
+	for i in range (1, len(id_range)):
+		interval = id_range[i]
+		# print((left,right))
+		# 1. Case they dont overlap. We have to add the sum (stop-start) and replace the interval
+		if interval[0] >= right:
+			sum += (right - left) 
+			left,right = (interval[0], interval[1] + 1)
+		# 2. They do partially overlap, find out if we need to expand interval or can keep it the same
+		if interval[1] + 1 > right:
+			right = interval[1] + 1
+	sum += (right - left)
+	return sum
+
+test_id_ranges = [(1,12), (2,15), (18,19)]
+print(calculate_total_fresh_ids(test_id_ranges))
+
+
 id_ranges = []
 ingredient_ids = []
 result = 0
@@ -43,6 +74,9 @@ with open(filepath, 'r') as f:
 		if check_ingredient_fresh(id, id_ranges):
 			result += 1
 		ingredient_ids.append(id)
-print(result)
+
+# For part II we need to sort the id_ranges
+id_ranges.sort(key=lambda x:x[0])
+print(calculate_total_fresh_ids(id_ranges))
 		
 		
