b31a38f7812c6cef08f40f9ed00f74b22a7f7975
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2025-12-24 14:05:23 +0100

    first attempt at 6.2 conceptually good but buggy

--- AdventOfCode2025/day6.py
+++ AdventOfCode2025/day6.py
@@ -1,8 +1,4 @@
 """
-1. We have to create a two grid array first
-"""
-
-"""
 This method performs the operation on all the numbers in the array
 The last field always contains the operator
 """
@@ -21,6 +17,16 @@ def part_one(calculation: list[str]) -> int:
 print(part_one(['6612', '1526', '583', '922', '+']))	
 print(part_one(['1', '2', '3', '4', '*']))
 
+"""
+For part two we now need to change how we read in the numbers, in particular instead of reading in the normal rows we now need to construct
+the calculation rows by reading each column multiple times, meaning that we read them over an over per column to construct the numbers for
+the calculation properly. 
+Ex: We have a column 31, 12, 431
+	-> Read column once: only 4, read twice 123, read three times 314, need it 
+The good thing: Since we perform the same operation on all number we build from this column due to cummutative property, it does not matter
+in which order we actually build the numbers, so we can literally start from the front of the number and then move backwards.  
+"""
+
 ops_array = []
 filepath = '/Users/jonas/Downloads/input-6.txt'
 with open(filepath, "r") as f:
@@ -29,11 +35,26 @@ with open(filepath, "r") as f:
 		ops_array.append(line.strip().split())
 
 result = 0
+ops_array = [
+	['123', '328', '51', '64'],
+	['45', '64', '387', '23'],
+	['6', '98', '215', '314'],
+	['*', '+', '*', '+']
+]
+
 for i in range(len(ops_array[0])):
 	calculation = []
+	numbers = []
 	for j in range(len(ops_array)):
-		calculation.append(ops_array[j][i])
+		offset = len(numbers) - len(ops_array[j][i]) # The length of the offset we need for adding the word
+		for l in range(len(ops_array[j][i])):
+			if l >= (len(numbers) - 1): # The last field contains the operator that must be seperated 
+				numbers.append(ops_array[j][i][l])
+			else:
+				numbers[l + offset] += ops_array[j][i][l]
+		print(numbers)
+		#calculation.append(ops_array[j][i])
 	print(calculation)
-	result += part_one(calculation)
-print(result)
+	#result += part_one(calculation)
+#print(result)
 #print(ops_array[len(ops_array)-1])
