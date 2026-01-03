c43e4e4f5f305e94c228f6113c5810504f9503e2
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-03 19:55:05 +0100

    first full code for 7.1 doesnt work, bug

--- AdventOfCode2025/day7.py
+++ AdventOfCode2025/day7.py
@@ -20,7 +20,7 @@ Which behaviors do I need to be aware of?
 
 Edge cases: Check if out of bounds, then nothing happens
 """
-def simulate_beams(diagram: list[list[str]]) -> list[list[str]]:
+def simulate_beams(diagram: list[list[str]]) -> (int, list[list[str]]):
 	beam_counter = 0
 	# Go through each line
 	for i in range(len(diagram)): 
@@ -29,25 +29,36 @@ def simulate_beams(diagram: list[list[str]]) -> list[list[str]]:
 			if diagram[i][j] == '^':
 			# If splitter:
 				# We check if splitter has incoming beam on top of it
-				if i > 0 and diagram[i-1][j]: 
+				if i > 0 and diagram[i-1][j] == '|': 
+					beam_counter += 1
 					# If yes -> We check if left, right has empty space
-						# Increase splitt counter by 1 
-						# If empty space -> replace with light beam 
+					# If empty space -> replace with light beam
+					if j - 1 >= 0 and diagram[i][j-1] == '.':
+						diagram[i][j-1] = '|'
+					if j + 1 < len(diagram[i]) and diagram[i][j+1] == '.':
+						diagram[i][j+1] = '|'
 			# If Empty space:
 			elif diagram[i][j] == '.':
 				# We check is space above has beam:
+				if i > 0 and diagram[i-1][j] == '|':
 					# If yes change field to beam to pass it down
-			# If beam: Nothing
+					diagram[i][j] == '|'
 			# Remember to check for S (start point in first row)
+			if i == 0 and diagram[i][j] == 'S':
+				diagram[i][j] == '|'
+			# If beam: Nothing
 
- 	 # return changes diagram
+	return (beam_counter,diagram)
 	
 
 
 filepath = '/Users/jonas/Downloads/input-7.txt'
-
+diagram = []
 with open(filepath, 'r') as file:
 	for line in file:
-		print(line.strip())
+		diagram.append(list(line.strip()))
 
+result, diagram = simulate_beams(diagram)
+print(result)
+	
 
