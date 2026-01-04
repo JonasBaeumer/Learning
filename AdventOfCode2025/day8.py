f70374fa130c0bab455f9830cee356aebe5e2960
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-04 15:16:53 +0100

    data transformation for 8.1

---
+++ AdventOfCode2025/day8.py
@@ -0,0 +1,36 @@
+"""
+The problem requires careful consideration of data structures, in particular we need two datastructures that:
+1) Allows to quickly calculate and determine the distance between pairs (and also allows us to find the 1000 lowest pairs
+2) We can use to track our current circuits (we start with single circuits) and then continously merge circles (as well as
+look up if specific pairs are already part of a respective circuit
+"""
+
+"""
+Approach: 
+Since we only have approx 1000 data points, we can brute force compute all of our distances. While doing so we keep a max heap of 1000
+shortest pairs.
+
+Since we are using a max heap our root (the max node) is actually the worst of the 1000 elements. This way we can easily determine
+wether a new distance should be inserted into the tree or not (only if < root). 
+For the first 1000 distance we directly push them into the tree.  
+"""
+#def find_1000_shorted_pairs() -> list[(int, int, int)]:
+	
+	
+
+"""
+"""
+#def union_merge():
+
+filepath = '/Users/jonas/Downloads/input-8.txt'
+datapoints = []
+
+with open(filepath, "r") as file:
+	counter = 0
+	for line in file:
+		counter += 1
+		coordinates = line.strip().split(",")
+		coordinates = list(map(lambda x: int(x), coordinates))
+		datapoints.append((coordinates[0], coordinates[1], coordinates[2]))
+
+print(datapoints)
