70043ffde88ca7d7bd289e8f3c6ec359baf06e58
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-04 16:07:09 +0100

    working shorted 1000 pairs implementation using min heap

--- AdventOfCode2025/day8.py
+++ AdventOfCode2025/day8.py
@@ -7,7 +7,7 @@ look up if specific pairs are already part of a respective circuit
 
 import math
 # Helper function to calculate the Euclidean distance for 3d points:
-def euclidean_distance(a, b): (int, int, int), (int, int, int) -> int:
+def euclidean_distance(a: tuple[int, int, int], b: tuple[int, int, int]) -> int:
 	return math.dist(a, b)
 
 """
@@ -19,9 +19,20 @@ Since we are using a max heap our root (the max node) is actually the worst of t
 wether a new distance should be inserted into the tree or not (only if < root). 
 For the first 1000 distance we directly push them into the tree.  
 """
-#def find_1000_shorted_pairs() -> list[(int, int, int)]:
-	
-	
+import heapq
+
+def find_1000_shortest_pairs(points: list(tuple[int, int, int])) -> list[(tuple[int, int, int], tuple[int, int, int])]:
+	heap = []
+	for i in range(len(points)):
+		for j in range(i + 1, len(points)):
+			distance = euclidean_distance(points[i], points[j])
+			# while we have less than 1000 distances stored we always add
+			if len(heap) < 1000:
+				heapq.heappush(heap, (-distance, points[i], points[j]))
+			else:
+				if distance < -heap[0][0]:
+					heapq.heapreplace(heap, (-distance, points[i], points[j]))
+	return [(-neg_d, a, b) for (neg_d, a, b) in heap]
 
 """
 Euclidean distance with 3d points: sqrt( (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2 )
@@ -40,4 +51,4 @@ with open(filepath, "r") as file:
 		coordinates = list(map(lambda x: int(x), coordinates))
 		datapoints.append((coordinates[0], coordinates[1], coordinates[2]))
 
-print(datapoints)
+print(find_1000_shortest_pairs(datapoints))
