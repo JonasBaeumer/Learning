db9ed3c77e7b9809592f31c9baa072eb1c630a95
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-06 20:26:16 +0100

    concept for calculating surface of rectangle

--- AdventOfCode2025/day9.py
+++ AdventOfCode2025/day9.py
@@ -5,6 +5,19 @@ We then keep the max value.
 Question: How do I calculate the surface of a rectangle using two points?
 """
 
+def calculate_surface(point_a: tuple[int, int], point_b: tuple[int, int]) -> int
+	# First we need to calculate point_c and point_d
+
+	# And then we can calculate the length of two adjacent edges and when we then calculate a * b
+	# and that gives us the total surface or the rectangle
+	# width  = |x2 - x1|
+        # height = |y2 - y1|
+        # area = |x2 - x1| * |y2 - y1|
+        width = abs(
+
+def find_max_rectangle(tiles: list(tuple[int, int])) -> int:
+
+
 tiles = []
 filepath = '/Users/jonas/Downloads/input-9.txt'
 with open(filepath, "r") as file:
