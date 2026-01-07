ef77c7d804085d4a26607c28f16e0b072731ccdc
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-07 14:57:38 +0100

    conceptual approach for 9.2

--- AdventOfCode2025/day9.py
+++ AdventOfCode2025/day9.py
@@ -3,6 +3,18 @@ First idea: We can mathematically calculate the actual size of the rectacle by c
 We then keep the max value.
 
 Question: How do I calculate the surface of a rectangle using two points?
+
+Approach for Part II: The key idea is not that dont always have a valid triangle formed between any arbitrary two points, rather
+we have specific conditions that fill space. Therefore the new approach is the following:
+1) Using the initial list of points in order, we calculate all the green points between them and put them in a set for fast lookup
+	Rule: Every two adjacent points in the list from a valid line in between them. 
+2) When we now want to check which red points would form a valid triangle, we now have to check wether the complete out border 
+of that rectangle is filled with red (or green) points. If yes, then we can deduce the interior is as well and the rectangle 
+is a valid candidate 
+	Open question: How do we identify red points along the line since they are also valid along the line of two red points
+	Proposal: Add the red points to the green point list as well since being red doesnt give us any special advantage when we dont
+	look at the respective points as part of the two opposite red edge points
+3) Go through the list of all candidates and calculate the max surface (similar to part I)
 """
 
 def calculate_surface(point_a: tuple[int, int], point_b: tuple[int, int]) -> int:
@@ -25,10 +37,10 @@ def find_max_rectangle(tiles: list(tuple[int, int])) -> int:
 	return max_value
 
 
-tiles = []
+red_tiles = []
 filepath = '/Users/jonas/Downloads/input-9.txt'
 with open(filepath, "r") as file:
 	for line in file:
 		coordinates = line.strip().split(',')
-		tiles.append((int(coordinates[0]), int(coordinates[1])))
-print(find_max_rectangle(tiles))
+		red_tiles.append((int(coordinates[0]), int(coordinates[1])))
+print(find_max_rectangle(red_tiles))
