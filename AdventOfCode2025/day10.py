ae0da4f1719ea2890f522b4e92448b45f414994b
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-09 18:46:59 +0100

    fixing some bugs in bfs exploration for 10.2

--- AdventOfCode2025/day10.py
+++ AdventOfCode2025/day10.py
@@ -71,11 +71,10 @@ def find_shortest_path_for_machine(goal_state: str, instructions: list[list[int]
 	return -1
 
 def _increase_joltage(joltage: list[int], instruction: list[int]):
-	joltage = joltage 
-	for i in range(len(joltage)):
-		if i in instruction:
-			joltage[i] += 1
-	return joltage
+	new_joltage = joltage.copy() 
+	for idx in instruction:
+		new_joltage[idx] += 1
+	return new_joltage
 
 print(_increase_joltage([0,0,0], [0,2]))
 print(_increase_joltage([0,0,0], [0,1]))
@@ -93,19 +92,41 @@ def find_shortest_path_to_joltage_state(goal_state: list[int], instructions: lis
 	queue.append((start_state, 1))
 	while queue:
 		state, index = queue.popleft()
-		visited.add((state, index))
+		state_tuple = tuple(state)
+		if state_tuple in visited:
+			continue
+		visited.add(state_tuple)
 
-		#for instruction in instructions:
-	return None
+		for instruction in instructions:
+			new_state = _increase_joltage(state, instruction)
+			new_state_tuple = tuple(new_state)
+			if new_state == goal_state:
+				return index
+			else:
+				for any(new_state[i] > goal_state[i] for i in range(len(new_state))):
+					continue 
+				if new_state_tuple not in visited:
+					visited.add(state_tuple)
+					queue.append((new_state, index + 1))
+	return -1
 
 
-print(find_shortest_path_for_machine("###", [[0], [1], [2]]))
-print(find_shortest_path_for_machine(".###.#", [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]]))
+print(find_shortest_path_to_joltage_state([1,1,1], [[0], [1], [2]]))
+print(find_shortest_path_to_joltage_state([3,5,4,7], [[3], [1,3], [2], [2,3], [0,2], [0,1]]))
+
+
 
 def first_part(machines: list[str]):
 	result = 0
 	for machine in machines:
 		result += find_shortest_path_for_machine(machine[0], machine[1])
+	return result
+
+def second_part(machine: list[str]):
+	result = 0
+	for machine in machines:
+		print(result)
+		result += find_shortest_path_to_joltage_state(machine[2], machine[1])
 	return result
 
 import re
@@ -119,6 +140,6 @@ with open(filepath, 'r') as file:
 		machines.append(_parse_string(line.strip()))
 print(machines[0])
 print(len(machines))
-print(first_part(machines))
+print(second_part(machines))
 		
 		
