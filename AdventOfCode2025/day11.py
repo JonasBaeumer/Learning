ec4c0c0b1e29f096e331bd0ab6f6af277be74d88
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-11 18:42:29 +0100

    added graph exploration method for finding all paths 10.1

--- AdventOfCode2025/day11.py
+++ AdventOfCode2025/day11.py
@@ -21,6 +21,44 @@ def build_graph(paths: list[str]):
 		graph[key] = values
 	return graph	
 	
+"""
+This method traverses the tree and find all the available paths to target with DFS
+"""
+def find_paths_to_end(graph: dict[str, list[str]]):
+
+	def dfs(node: str, visited: list[str]):
+		# base case: we found a way to "end"
+		if not node:
+			return 0
+		# base case: we are running in a cycle and have already seen this element
+		if path in visited:
+			return 0
+		# base case: we have actually found a valid path
+		if node == "end":
+			return 1
+		paths = graph[node]
+		result = 0
+		for path in paths:
+			visited.append(path)
+			result += dfs(path, visited)
+		return result
+	
+	return dfs("you", [])
+
+test_graph = {
+    "aaa": ["you", "hhh"],
+    "you": ["bbb", "ccc"],
+    "bbb": ["ddd", "eee"],
+    "ccc": ["ddd", "eee", "fff"],
+    "ddd": ["ggg"],
+    "eee": ["out"],
+    "fff": ["out"],
+    "ggg": ["out"],
+    "hhh": ["ccc", "fff", "iii"],
+    "iii": ["out"]
+}
+
+print(find_paths_to_end(test_graph))
 
 filepath = "/Users/jonas/Downloads/input-11.txt"
 lines = []
