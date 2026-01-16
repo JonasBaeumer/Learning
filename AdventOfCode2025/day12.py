2dec83c47688e5e8b5d1a3547bbeaf2f87d15fd4
JonasBaeumer <71970468+JonasBaeumer@users.noreply.github.com>
2026-01-16 15:51:03 +0100

    working solution for 12

--- AdventOfCode2025/day12.py
+++ AdventOfCode2025/day12.py
@@ -2,7 +2,27 @@
 1) Split the input data apart into the grids and shapes sections
 	grid 	-> List[List[str]]
 	regions -> tuples(int, int, List[int])]
+
+2) Use some form of backtracking / DFS algorithm to solve this 
+
+2a) First supapproach, we can narrow down the list of available fields to check, even if all shapes would fit perfectly with no breaks, 
+would there even be enough space in the grid to place them?
 """
+
+"""
+This method determines wether a field even has enough place to fit the required shapes if there are all spaces perfectly (no gaps)
+"""
+def _enough_available_space(a: int, b: int, quantities: list[int]) -> bool:
+	space = [5, 7, 7, 7, 7, 7]
+	area = a * b
+	sum = 0
+
+	for i in range(len(quantities)):
+		sum += (quantities[i] *	space[i])
+	
+	return sum <= area
+
+
 
 filepath = "/Users/jonas/Downloads/input-12.txt"
 
@@ -23,7 +43,6 @@ with open(filepath, "r") as file:
 	grid_list = []
 
 	for line in file:
-		print(line.strip())
 		if not line:
 			continue
 		
@@ -46,5 +65,8 @@ with open(filepath, "r") as file:
 			h = int(m.group(2))
 			counts = list(map(int, m.group(3).split()))
 			regions.append((w, h, counts))	
-
-print(grid)
+			print(_enough_available_space(w, h, counts))
+sum = 0
+for region in regions:
+	sum += _enough_available_space(region[0], region[1], region[2])
+print(sum)
