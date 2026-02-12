e3c6c9ecbef4614fe7f81439212f560b1fce8110
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-02-12 14:00:47 +0100

    intermediate commit

--- go/function.go
+++ go/function.go
@@ -12,7 +12,20 @@ func add_shorted_params(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// Naked returns: We may name the return values in the function header, if we just return at the end the named return 
+// values will be returned 
+// That means the methods are automatically defined during default initialization
+func split(sum int) (x, y string) {
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 12))
 	fmt.Println(add_shorted_params(42, 12))
+	fmt.Println(swap("hello", "world"))
+	fmt.Println(split(10))
 }
