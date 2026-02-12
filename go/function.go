198f61d02549ab314d0aba7798c6aef318862499
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-02-12 13:55:36 +0100

    intermediate commit

--- go/function.go
+++ go/function.go
@@ -6,6 +6,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+// When two or more consecutive parameters share the same type we can emit all the types 
+// except the last one 
+func add_shorted_params(x, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println(add(42, 12))
+	fmt.Println(add_shorted_params(42, 12))
 }
