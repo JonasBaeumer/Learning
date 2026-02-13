6f0cae93deb3478e4ca9bdd0feb004612791b960
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-02-13 11:28:46 +0100

    finished lesson about control flow

--- go/ATourOfGo/defer.go
+++ go/ATourOfGo/defer.go
@@ -7,7 +7,13 @@ import "fmt"
 // The deferred calls arguments are evaluated imediately but the function call is not executed until the sourrounding function return 
 // We can use this for example to handle behavior in case something crashes, for example to close the database connection
 
+// Defer calls are pushed to a stack meaning they will be executed in LAFO (last in first out order) when the function has terminated
+
 func main() {
 	defer fmt.Println("Dont forget to close the database connection")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
 	fmt.Println("Doing something")
 }
