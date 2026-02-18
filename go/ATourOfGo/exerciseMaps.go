2dd312da071922f5c59313c613be7d50a097ebe8
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-02-18 14:35:07 +0100

    finished exercise

---
+++ go/ATourOfGo/exerciseMaps.go
@@ -0,0 +1,28 @@
+// Implement WordCount. It should return a map of the counts of each “word” in the string s. 
+// The wc.Test function runs a test suite against the provided function and prints success or failure.
+
+// You might find strings.Fields helpful.
+
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	wordCount := make(map[string]int)
+	for i := 0; i < len(s); i++ {
+		if _, ok := wordCount[string(s[i])]; ok {
+			wordCount[string(s[i])] += 1
+		} else {
+	 	 	wordCount[string(s[i])] = 1
+		}
+	}
+	fmt.Println(wordCount)
+	return wordCount
+}
+
+func main() {
+	wc.Test(WordCount)
+}
