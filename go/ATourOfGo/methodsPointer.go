4f82522fbdf8dadfa7ad73ea7f31b09db2b7f24c
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-02-27 17:36:56 +0100

    methodsPointer.go

--- go/ATourOfGo/methodsPointer.go
+++ go/ATourOfGo/methodsPointer.go
@@ -26,10 +26,30 @@ func (v *Vertex) Scale(f float64) {
 // That is when we dont call the pointer scale would receive a copy of the underlying v
 // object, since we dont return a value and subsequently overwrite it the copy just 
 // gets lost and therefore nothing changes
-// func (v *Vertex) Scale(f float64) {
+// func (v Vertex) Scale(f float64) {
 //	v.X = v.X * f
 //	v.Y = v.Y * f 
 // }
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+// What would happen if we call a method with pointer receiver with a pointer and vice versa?
+// var v Vertex
+// ScaleFunc(v, 5)  // Compile error!
+// ScaleFunc(&v, 5) // OK
+
+// while methods with pointer receivers take either a value or a pointer as the receiver when they are called:
+// var v Vertex
+// v.Scale(5)  // OK
+// p := &v
+// p.Scale(10) // OK
+
+// In General when there is function arguments the function arguments must always exactly match 
+// the specifified type in the argument e.g. func(*T) must provide something of type *T, while 
+// func(T) must provide type T
+
 
 func main () {
 	v := Vertex{3, 4}
