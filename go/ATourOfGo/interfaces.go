5adfac1d79a34d20431ff086a2d3466a395a0cf7
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-03-03 10:45:12 +0100

    interfaces.go

--- go/ATourOfGo/interfaces.go
+++ go/ATourOfGo/interfaces.go
@@ -52,4 +52,40 @@ func main() {
 	// We can also declare an empty interface which can then handle any arbitrary type 
 	// This is usefull when handling unknown values 
 	var i interface{}
+
+	// Type assertions: when wrapped / declared in an interface we can not directly acess the underlying type 
+	// That is problematic because we can not access function that would work on the original type but will throw an error 
+	// for when being called on the interface 
+	// Calling that will trigger a panic -> A runtime crash the programm stops executing, really bad!
+
+	// Key role:
+  	// Type assertions only work on interface types. You can't assert on a concrete type directly — there's nothing to unwrap.
+	// You already know if the type is a string because that is induced at compile time or explicitly declared 
+	// If like saying to the compiler, check if this string is a string - doesnt make sense
+
+	var j interface{} := "Hello World"
+	// j.ToUpper() -> j i an interface not a string! Will trigger a panic / crash (bad)
+
+	// To check what the interface contains you can do a safe assertion
+	// Essentially we are asking: "Give me the value if it exists, tell me if it does not"
+	s, ok := j.(string)
+	fmt.Println(j, ok)
+
+	ints, ok := j.(int)
+	fmt.Println(ints, ok) // Will return false, but wont crash because we safe assert
+
+	// If we have multiple possible time, instead of chaining if statements together we can use SWITCH CASE 
+	// which is much more elegant 
+	
+	switch v := j.(type) {
+	case int:
+		fmt.Printf("int: %d\n", v)
+	case string:
+		fmt.Printf("string: %q\n", v)
+	default:
+		fmt.Printf("unknown type: %T\n", v)
+	}
+
+
+
 }
