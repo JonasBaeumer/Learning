f1210d0fb602e90a7af0cf90e48f2a5744c20570
Jonas Baumer <jonas.baumer@datadoghq.com>
2026-03-04 14:24:58 +0100

    error.go

--- go/ATourOfGo/error.go
+++ go/ATourOfGo/error.go
@@ -5,6 +5,10 @@ import (
 	"strconv"
 )
 
+func Sqrt(x float64) (float64, error) {
+	return 0, nil
+}
+
 func main() {
 
 	// Go functions express an error state with the error variable 
@@ -25,6 +29,15 @@ func main() {
 		fmt.Printf("couldn't convert number: %v\n", err)
     	return
 	}
-	
+
 	fmt.Println("Converted integer:", j)
+
+	nbr, err := Sqrt(-2)
+
+	if err != nil {
+		fmt.Printf("Couldn't draw square root from number: %v\n", err)
+		return
+	}
+
+	fmt.Println("Square root:", nbr)
 }
